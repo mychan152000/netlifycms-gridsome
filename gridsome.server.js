88a0dad00bf71d2d99d0fd061397fa45d9e4cd1f
unknown <jmckenzie152000@Gmail.com>
2023-07-16 20:04:44 +0700

    update

--- gridsome.server.js
+++ gridsome.server.js
@@ -6,7 +6,18 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = function (api) {
-  api.loadSource(({ addCollection }) => {
-    // Use the Data store API here: https://gridsome.org/docs/data-store-api/
+  api.loadSource(({ addCollection, store }) => {
+    // Use the Data Store API here: https://gridsome.org/docs/data-store-api/
+    const posts = addCollection({ typeName: 'Post' })
+    posts.addNode({
+      title: 'The post',
+      author1: store.createReference('Author', '1')
+    })
+
+    const authors = addCollection({ typeName: 'Author' })
+    authors.addNode({
+      id: '1',
+      title: 'The author'
+    })
   })
 }
